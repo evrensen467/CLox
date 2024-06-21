b1d7777aea9106207f86642d9388566c32b41f2b
Evren Sen <contact@evrsen.dev>
2024-06-21 16:24:42 +0200

    Initial Commit

---
+++ CLox/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CLox
+//
+//  Created by Evren Sen on 2024-06-21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
